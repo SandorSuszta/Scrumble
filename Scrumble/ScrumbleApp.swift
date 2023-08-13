455699a4e6d4c5f9cc31bd54e3d60c35aedc7f99
Sandor Suszta <sasha.shusta@gmail.com>
2023-08-13 23:09:55 +0100

    Initial Commit

---
+++ Scrumble/ScrumbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumbleApp.swift
+//  Scrumble
+//
+//  Created by Nataliia Shusta on 13/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
